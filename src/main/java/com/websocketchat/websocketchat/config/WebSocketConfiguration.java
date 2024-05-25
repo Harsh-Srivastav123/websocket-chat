6b2f740bcda12075d3ff56e886068b26dcf5d99e
Harsh Raj Srivastav <114308429+Harsh-Srivastav123@users.noreply.github.com>
2024-05-25 15:20:48 +0530

    Update WebSocketConfiguration.java

--- src/main/java/com/websocketchat/websocketchat/config/WebSocketConfiguration.java
+++ src/main/java/com/websocketchat/websocketchat/config/WebSocketConfiguration.java
@@ -18,7 +18,7 @@ import java.util.List;
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/server").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/server").withSockJS();
     }
 
     @Override
